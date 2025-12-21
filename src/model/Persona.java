4780ddd8e90ba3ddc121bb18819e7cfee02e24df
Sebastián Rojo <sebastianrojocollao@gmail.com>
2025-12-20 22:44:22 -0300

    Agregar clase padre

---
+++ src/model/Persona.java
@@ -0,0 +1,21 @@
+package model;
+
+public abstract class Persona {
+    
+    private String rut;
+    private String nombre;
+
+    public Persona(String rut, String nombre) {
+        this.rut = rut;
+        this.nombre = nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+}
