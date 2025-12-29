52a36705d5a2590e1e5c7b05feeb6ab1ae27b8d6
Sebastián Rojo <sebastianrojocollao@gmail.com>
2025-12-28 22:54:09 -0300

    Agregar modelo de inscripción

---
+++ src/model/Inscripcion.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Inscripcion {
+
+    private Estudiante estudiante;
+
+    public Inscripcion(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+}
